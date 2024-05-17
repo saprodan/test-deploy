5e64543938088c0b26843210bf78937c14834909
saprodan <111009303+saprodan@users.noreply.github.com>
2024-05-17 14:11:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/card-shop' : '',
+  // basePath: isProd ? '/card-shop' : '',
   output: 'export',
 };
 
