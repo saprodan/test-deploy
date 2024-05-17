359e42274ee475fcdb2d645b57ffca4c852128c7
saprodan <saprodan@2100.gmail.com>
2024-05-17 13:57:41 +0300

    простая страница с навигацией

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  basePath: isProd ? '/card-shop' : '',
+  output: 'export',
+};
 
 export default nextConfig;
